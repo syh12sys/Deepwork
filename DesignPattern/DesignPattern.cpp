64cdd72e617ccb2fa31dc3e5937c3ac56cfa706b
syh12sys <sunys@trump.local>
2019-10-09 08:47:40 +0800

    设计模式-测试用例

--- DesignPattern/DesignPattern.cpp
+++ DesignPattern/DesignPattern.cpp
@@ -17,6 +17,22 @@
 #include "singleton/Singleton.h"
 #include "singleton/LazySingleton.h"
 
+#include "command/CommandClient.h"
+
+#include "adapter/AdapterTest.h"
+
+#include "facade/facade.h"
+
+#include "template/templateTest.h"
+
+#include "iterate/Waiter.h"
+
+#include "composite/ComponentTest.h"
+
+#include "proxy/RemoteProxy/RemoteProxyTest.h"
+
+#include "proxy/VirtualProxy/VirtualProxyTest.h"
+
 int _tmain(int argc, _TCHAR* argv[])
 {
     Subject subject;
@@ -47,6 +63,40 @@ int _tmain(int argc, _TCHAR* argv[])
     std::cout << "\n********************单例模式，最简单却最有用************************" << std::endl;
     Singleton::GetInstance();
     LazySingleton::GetInstance();
+
+    std::cout << "\n********************命令模式，这么多年一直没理解的模式，通过实例现在终于理解了************************" << std::endl;
+    CommandClient commandClient;
+    commandClient.Test();
+
+    std::cout << "\n********************适配器/包装器模式，很有用但是一直忽视的模式************************" << std::endl;
+    AdapterTest adapterTest;
+    adapterTest.StartPlay();
+
+    std::cout << "\n********************门面模式，最容易在代码出现的一种模式************************" << std::endl;
+    Facade facade;
+    facade.WatchTV();
+    facade.CloseTV();
+
+    std::cout << "\n********************模板模式，自己最喜欢的去重复代码的模式************************" << std::endl;
+    TemplateTest templateTest;
+    templateTest.Test();
+
+
+    std::cout << "\n********************迭代器模式，封装遍历行为************************" << std::endl;
+    Waiter waiter;
+    waiter.PrintAllMenu2();
+
+    std::cout << "\n********************组合模式，单一职责换取透明性************************" << std::endl;
+    ComponentTest componentTest;
+    componentTest.Test();
+
+    std::cout << "\n********************远程代理，多进程通信的必备模式************************" << std::endl;
+    RemoteProxyTest remoteProxyTest;
+    remoteProxyTest.Test();
+
+    std::cout << "\n********************虚拟代理，降低资源消耗的工具************************" << std::endl;
+    VirtualProxyTest virtualProxyTest;
+    virtualProxyTest.Test();
 
     getchar();
 	return 0;
